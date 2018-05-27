88c0d007ebc3db819bcac6943b95dc9275351038
lixiang <lixiang9409@vip.qq.com>
2018-05-27 22:51:10 +0800

    添加了gitignore

--- tuchuang-admin/src/main/java/mini/ideashare/tlbb/Main.java
+++ tuchuang-admin/src/main/java/mini/ideashare/tlbb/Main.java
@@ -1,14 +1,9 @@
-package tech.ideashare.tuchuangadmin;
+package mini.ideashare.tlbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-package tech.ideashare.tuchuangadmin;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @Author lixiang
