6aa9b312aa41c91a7d65c882e0257a1530c44957
luomiaom <luomiaomiaommm@163.com>
2018-07-10 16:24:44 +0800

    添加了用户插入

--- tuchuang-admin/src/main/java/mini/ideashare/cms/controller/UserController.java
+++ tuchuang-admin/src/main/java/mini/ideashare/cms/controller/UserController.java
@@ -4,6 +4,7 @@ import mini.ideashare.cms.manager.UserManager;
 import mini.ideashare.cms.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ public class UserController {
     @Autowired
     private UserManager userManager;
     @PostMapping("/user/saveUser")
-    public int insert(User user){
+    public int insert(@RequestBody  User user){
         return userManager.insert(user);
     }
 }
